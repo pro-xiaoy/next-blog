3b20ba64648f851af7623066b463f9db4a85b7fc
fangyangyang <fangyangyang@mcep.net.cn>
2021-04-22 11:31:17 +0800

    feat 强行改下文章

--- pages/posts/[title].tsx
+++ pages/posts/[title].tsx
@@ -1,8 +1,13 @@
 import { getPost } from 'lib/posts'
 import { NextPage } from 'next'
 
+type Context = {
+  params: {
+    title: String;
+  }
+}
 
-const pageTitle: NextPage<{posts: Posts}> = (props) => {
+const pageTitle: NextPage<{ posts: Posts }> = (props) => {
   const { posts } = props
   return (
     <div>
@@ -19,14 +24,17 @@ export default pageTitle
 export async function getStaticPaths() {
   return {
     paths: [
-      { params: { title: 'first' } }
+      { params: { title: 'first' } },
+      { params: { title: 'second' } }
+
     ],
     fallback: false,
   }
 }
 
-export const getStaticProps = async () => {
-  const posts = await getPost('first')
+export const getStaticProps = async (context: Context) => {
+  const title = context.params.title
+  const posts = await getPost(title)
   return {
     props: {
       posts: posts,
