dbb69234676c83b73d928b47b79b387917cec7d5
fangyangyang <fangyangyang@mcep.net.cn>
2021-04-23 09:04:19 +0800

    fix Post is no matterdata

--- dist/index.js
+++ dist/index.js
@@ -10,27 +10,30 @@ require("reflect-metadata");
 
 var _typeorm = require("typeorm");
 
+var _Post = require("./entity/Post");
+
 (0, _typeorm.createConnection)().then( /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(connection) {
+    var p;
     return _regenerator["default"].wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            // console.log("Inserting a new user into the database...");
-            // const user = new User();
-            // user.firstName = "Timber";
-            // user.lastName = "Saw";
-            // user.age = 25;
-            // await connection.manager.save(user);
-            // console.log("Saved a new user with id: " + user.id);
-            // console.log("Loading users from the database...");
-            // const users = await connection.manager.find(User);
-            // console.log("Loaded users: ", users);
-            // console.log("Here you can setup and run express/koa/any other framework.");
+            // const posts = await connection.manager.find(Post)
+            p = new _Post.Post();
+            p.title = 'Post 1';
+            p.content = '我的第一篇文章';
+            _context.next = 5;
+            return connection.manager.save(p);
+
+          case 5:
+            console.log('p+++', p); // console.log('Post+++', Post)
+            // const posts = await connection.manager.find(Post);
+
             console.log('connection+++', connection);
             connection.close();
 
-          case 2:
+          case 8:
           case "end":
             return _context.stop();
         }
