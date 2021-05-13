9598805aed2f2254095f7b3d9a7b965181b97ebd
fangyangyang <fangyangyang@mcep.net.cn>
2021-05-13 18:22:54 +0800

    feat 首页完成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,5 @@
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
-import png from 'assets/images/1.png'
-import { GetServerSideProps } from 'next'
-import { getDatabaseConnection } from '../lib/getDatabaseConnection'
-import { Post } from 'src/entity/Post'
+import postNew, { getServerSideProps } from 'pages/posts'
 
-export default function Home(props) {
-  const { posts } = props
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>博文文章</h1>
-      {posts.map(item => (
-        <div>
-          <Link key={item.id} href={`/posts/${item.id}`}>
-            <a>
-              {item.title}
-            </a>
-          </Link>
-        </div>
-      ))}
-    </div>
-  )
-}
+export default postNew
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const connection = await getDatabaseConnection()
-  const posts = await connection.manager.find(Post)
-  return {
-    props: {
-      name: 'xiaoy',
-      posts: JSON.parse(JSON.stringify(posts)),
-    }
-  }
-}
+export { getServerSideProps }
